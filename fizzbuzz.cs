bbb680e5ee4379b81cc5aacf24551f6c4d43bd40
Prayoch Rujira <jay@odd-e.co.th>
2015-09-05 21:17:05 +0700

    use forEach instead of hardcode array index

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -3,13 +3,13 @@ public class FizzBuzz()
     public string Say(int arabic)
     {
         []Rule rules = new []Rule{ new BuzzRule(), new FizzRule()};
-        if( rules[0].isAccept(arabic) )
+        
+        forEach(Rule rule in rules)
         {
-            return rules[0].Say();
-        }
-        if( rules[1].isAccept(arabic) )
-        {
-            return rules[1].Say();
+            if ( rule.IsAccept(arabic) )
+            {
+                return rule.Say();
+            }
         }
         return arabic.ToString();
     }
