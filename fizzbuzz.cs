60edb7e78f3d2be3dc80bc9d73e62048846a9689
Prayoch Rujira <jay@odd-e.co.th>
2015-09-05 21:15:20 +0700

    replace fizzRule with instance from rules

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -2,15 +2,14 @@ public class FizzBuzz()
 {
     public string Say(int arabic)
     {
-        []Rule rules = new []Rule{ new BuzzRule()};
+        []Rule rules = new []Rule{ new BuzzRule(), new FizzRule()};
         if( rules[0].isAccept(arabic) )
         {
             return rules[0].Say();
         }
-        FizzRule fizzRule = new FizzRule();
-        if( fizzRule.isAccept(arabic) )
+        if( rules[1].isAccept(arabic) )
         {
-            return fizzrule.Say();
+            return rules[1].Say();
         }
         return arabic.ToString();
     }
