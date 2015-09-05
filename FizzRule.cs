1d05739943ffc5518e3e89f40c026b2213ccde09
Prayoch Rujira <jay@odd-e.co.th>
2015-09-05 21:36:05 +0700

    replace or multiple conditions with mod

--- FizzRule.cs
+++ FizzRule.cs
@@ -2,7 +2,7 @@ public class FizzRule : Rule
 {
     public bool IsAccept(int arabic)
     {
-        return  arabic == 3 || arabic == 6 || arabic == 9;
+        return  (arabic % 3) == 0;
     }
 
     public string Say(){
